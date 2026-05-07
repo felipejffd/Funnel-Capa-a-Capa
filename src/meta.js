df325aa0efac1ac2a1e03a05446a1b27b72edd96
Felipe <felipejffd@gmail.com>
2026-05-07 12:14:53 -0500

    Update ad account ID and server fixes

--- src/meta.js
+++ src/meta.js
@@ -1,5 +1,5 @@
 ﻿const axios = require('axios');
 const metaClient = axios.create({ baseURL: 'https://graph.facebook.com/' + (process.env.META_API_VERSION || 'v21.0'), timeout: 30000 });
 metaClient.interceptors.request.use((config) => { config.params = { ...config.params, access_token: process.env.META_ACCESS_TOKEN }; return config; });
-metaClient.interceptors.response.use((res) => res, (err) => { const e = err.response?.data?.error; if (e) { const error = new Error(e.message); error.code = e.code; error.type = e.type; error.status = err.response.status; return Promise.reject(error); } return Promise.reject(err); });
+metaClient.interceptors.response.use((res) => res, (err) => { const e = err.response?.data?.error; if (e) { console.error('[Meta Full Error]', JSON.stringify(err.response.data, null, 2)); const error = new Error(e.message); error.code = e.code; error.type = e.type; error.subcode = e.error_subcode; error.user_msg = e.error_user_msg; error.details = e; error.status = err.response.status; return Promise.reject(error); } return Promise.reject(err); });
 module.exports = metaClient;
